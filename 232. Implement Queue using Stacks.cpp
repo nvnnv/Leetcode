9dd8e72975d9fc640f7625df091544731c9bb523
nvnnv <876837608@qq.com>
2020-10-12 10:16:04 +0800

    Create 232. Implement Queue using Stacks.cpp

---
+++ 232. Implement Queue using Stacks.cpp
@@ -0,0 +1,48 @@
+class MyQueue {
+public:
+    /** Initialize your data structure here. */
+    MyQueue() {
+
+    }
+
+    /** Push element x to the back of queue. */
+    void push(int x) {
+        s1.push(x);
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    int pop() {
+        if (s2.empty())
+        {
+            while (!s1.empty())
+            {
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+        int s = s2.top();
+        s2.pop();
+        return s;
+    }
+
+    /** Get the front element. */
+    int peek() {
+        if (s2.empty())
+        {
+            while (!s1.empty())
+            {
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+        return s2.top();
+    }
+
+    /** Returns whether the queue is empty. */
+    bool empty() {
+        return s1.empty() && s2.empty();
+    }
+private:
+    stack<int> s1;
+    stack<int> s2;
+};
